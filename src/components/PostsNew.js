f1ce6927ebcc4948cac71f4b11d7f9c2ad073ba7
Sprazzeus <sprazzeus@gmail.com>
2017-08-11 19:24:54 +0800

    Set up

---
+++ src/components/PostsNew.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+class PostsNew extends Component {
+  render () {
+    return (
+      <div>Posts New</div>
+    )
+  }
+}
+
+export default PostsNew

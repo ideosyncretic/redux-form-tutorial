36b317d056d5e74d1125e348d073e5d71c60cb32
Angel Coco Chen <angel@nuisance.co>
2017-08-22 22:55:52 +0800

    Form validation on touch

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -22,13 +22,13 @@ class PostsIndex extends Component {
   render () {
     return (
       <div>
-        <div className='text-xs-right'>
-          <Link className='btn btn-primary' to='/posts/new'>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
             Add a Post
           </Link>
         </div>
         <h3>Posts</h3>
-        <ul className='list-group'>
+        <ul className="list-group">
           {this.renderPosts()}
         </ul>
       </div>

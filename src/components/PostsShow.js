f62702ca59c6f28595dd57f2679a3370cd33ffb9
Angel Coco Chen <angel@nuisance.co>
2017-08-23 23:46:49 +0800

    Linking posts index to single post and back, and optimize data fetching in PostsShow

--- src/components/PostsShow.js
+++ src/components/PostsShow.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { fetchPost } from '../actions'
 
 class PostsShow extends Component {
   componentDidMount () {
-    // React Router provides params object, of all the different wildcards used in the route url
-    const { id } = this.props.match.params
-    this.props.fetchPost(id) // we need to inject it from state to our component using mapStateToProps
+    // if post is not already loaded (by index)
+    if (!this.props.post) {
+      // React Router provides params object, of all the different wildcards used in the route url
+      const { id } = this.props.match.params
+      this.props.fetchPost(id) // we need to inject it from state to our component using mapStateToProps
+    }
   }
   render () {
     const { post } = this.props
@@ -17,6 +21,7 @@ class PostsShow extends Component {
 
     return (
       <div>
+        <Link to='/' className='btn btn-primary'>Back to Index</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
